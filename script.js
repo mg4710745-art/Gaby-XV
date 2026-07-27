a3d8aaa1d9915674c077076e0dd9e1d6b2ed7ae4
mg4710745-art <mg4710745@gmail.com>
2026-07-27 17:44:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -12,14 +12,30 @@ function abrirInvitacion(){
 
 }
 
-let numero = 1;
+letlet fotos=[
+"carrusel/FOTO 1.jpeg",
+"carrusel/FOTO 2.jpeg",
+"carrusel/FOTO 3.jpeg",
+"carrusel/FOTO 4.jpeg",
+"carrusel/FOTO 5.jpeg",
+"carrusel/FOTO 6.jpeg",
+"carrusel/FOTO 7.jpeg",
+"carrusel/FOTO 8.jpeg",
+"carrusel/FOTO 9.jpeg",
+"carrusel/FOTO 10.jpeg",
+"carrusel/FOTO 11.jpeg",
+"carrusel/FOTO 12.jpeg",
+"carrusel/FOTO 13.jpeg",
+"carrusel/FOTO 14.jpeg",
+"carrusel/FOTO 15.jpeg"
+]; numero = 0;
 
 setInterval(function(){
 
     numero++;
 
-    if(numero > 15){
-        numero = 1;
+    if(numero >= fotos.length){
+        numero = 0;
     }
 
     document.getElementById("fotoCarrusel").src =
