e276650edb1d303ee425e073d3250f4f5cef0bed
mg4710745-art <mg4710745@gmail.com>
2026-07-27 18:06:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 function abrirInvitacion() {
     alert("El botón funciona");
 }
+const fechaXV = new Date("2026-11-15T18:00:00");
+
+setInterval(function(){
+
+    const ahora = new Date();
+
+    const diferencia = fechaXV - ahora;
+
+    const dias = Math.floor(diferencia / (1000 * 60 * 60 * 24));
+
+    const horas = Math.floor((diferencia % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+
+    const minutos = Math.floor((diferencia % (1000 * 60 * 60)) / (1000 * 60));
+
+    const segundos = Math.floor((diferencia % (1000 * 60)) / 1000);
+
+    document.getElementById("dias").textContent = dias;
+    document.getElementById("horas").textContent = horas;
+    document.getElementById("minutos").textContent = minutos;
+    document.getElementById("segundos").textContent = segundos;
+
+},1000);
